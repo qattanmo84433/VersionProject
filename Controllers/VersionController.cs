021f19692bf810bed4d3bd1ca9854aaeebbf93e0
Mohamad Ali Qattan <mohamad-ali.qattan@adesso.de>
2022-09-21 13:22:51 +0200

    Add project files.

---
+++ Controllers/VersionController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VersionProject.Controllers;
+
+[Route("[controller]")]
+public class VersionController : Controller
+{
+    private const string Version = "3";
+
+    [HttpGet]
+    public IActionResult GetVersionNumber()
+    {
+        return Ok(Version);
+    }
+
+
+}
